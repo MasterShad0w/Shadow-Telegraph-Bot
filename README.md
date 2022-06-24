b0f5696b21ba0710a3806bf252ebb3d1f1bfa12e
Shadow King <105120735+MasterShad0w@users.noreply.github.com>
2022-06-24 06:39:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# TeleGraph-Uploader
+
+<p align="center">
+  <a href="https://www.python.org">
+    <img src="http://ForTheBadge.com/images/badges/made-with-python.svg" width ="250">
+  </a>
+  <a href="https://t.me/ShadowKing9o">
+    <img src="https://telegra.ph/file/93a7e33b65dc93349c0be.jpg" width="250">
+  </a><br>
+  <a href="https://t.me/ShadowsArena">
+    &nbsp;<img src="https://img.shields.io/badge/Shadow%20Arena-Channel-blue?style=plastic&logo=Telegram" width="130" height="18">&nbsp;
+  </a>
+  <a href="https://t.me/+9Zhp_GdQVctiNjc1">
+    &nbsp;<img src="https://img.shields.io/badge/Movie%20Addaa-Group-blue?style=plastic&logo=Telegram" width="130" height="18">&nbsp;
+  </a>
+  <br>
+  <a href="https://github.com/MasterShad0w/Shadow-Telegraph-Bot/stargazers">
+    <img src="https://img.shields.io/github/stars/MasterShad0w/Shadow-Telegraph-Bot?style=social">
+  </a>
+  <a href="https://github.com/MasterShad0w/Shadow-Telegraph-Bot/fork">
+    <img src="https://img.shields.io/github/forks/MasterShad0w/Shadow-Telegraph-Bot?label=Fork&style=social">
+  </a>  
+</p>
+
+## You can upload photos and videos to telegraph using this bot. Can be found in telegram as [ShadowTelegraphBot](https://t.me/ShadowTelegraphBot)
+
+### Deploy to Heroku 🏃‍♂
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/MasterShad0w/Shadow-Telegraph-Bot)
+
+### Credits:
+
+[Owner](https://t.me/ShadowKing9o)
+
+[Pyrogram Library](https://github.com/pyrogram/pyrogram)
+
+[Movie Group](https://t.me/+9Zhp_GdQVctiNjc1) 
+### Developers:
+
+[Shadow King](https://telegram.dog/ShadowKing9o)
---
+++ jebot.py
@@ -0,0 +1,162 @@
+import os
+import logging
+from pyrogram import Client, filters
+from telegraph import upload_file
+from config import Config
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+Jebot = Client(
+   "Telegraph Uploader",
+   api_id=Config.APP_ID,
+   api_hash=Config.API_HASH,
+   bot_token=Config.TG_BOT_TOKEN,
+)
+
+@Jebot.on_message(filters.command("start"))
+async def start(client, message):
+   if message.chat.type == 'private':
+       await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>Hey There, I'm Telegraph Bot
+
+I can upload photos or videos to telegraph. Made by @ShadowKing9o 🇮🇳
+
+Hit help button to find out more about how to use me</b>""",   
+                            reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Help", callback_data="help"),
+                                        InlineKeyboardButton(
+                                            "Movie Group", url="https://t.me/+9Zhp_GdQVctiNjc1")
+                                    ],[
+                                      InlineKeyboardButton(
+                                            "Support", url="https://t.me/ShadowsArena")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+
+@Jebot.on_message(filters.command("help"))
+async def help(client, message):
+    if message.chat.type == 'private':   
+        await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>Telegraph Bot Help!
+
+Just send a photo or video less than 5mb file size, I'll upload it to telegraph.
+
+~ @ShadowKing9o</b>""",
+        reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "Back", callback_data="start"),
+                                        InlineKeyboardButton(
+                                            "About", callback_data="about"),
+                                  ],[
+                                        InlineKeyboardButton(
+                                            "Source Code", url="https://github.com/MasterShad0w/Shadow-Telegraph-Bot")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+
+@Jebot.on_message(filters.command("about"))
+async def about(client, message):
+    if message.chat.type == 'private':   
+        await Jebot.send_message(
+               chat_id=message.chat.id,
+               text="""<b>About Telegraph Bot!</b>
+
+<b>👽 Creator :</b> <a href="https://t.me/ShadowKing9o">Shadow King</a>
+
+<b>🛑 Support :</b> <a href="https://t.me/ShadowsArena">Shadow Arena</a>
+
+<b>📔 Library :</b> <a href="https://github.com/pyrogram/pyrogram">Pyrogram</a>
+
+<b>🎥 Movies :</b> <a href="https://t.me/+9Zhp_GdQVctiNjc1">Movie Adda</a>
+
+<b>📡 Server :</b> <a href="www.heroku.com"> Heroku </a>
+
+<b>📝 Source : </b> <a href="https://github.com/MasterShad0w/Shadow-Telegraph-Bot"> Click Here </a>
+
+<b>~ @ShadowKing9o</b>""",
+     reply_markup=InlineKeyboardMarkup(
+                                [[
+                                        InlineKeyboardButton(
+                                            "🥷Owner🥷", url="https://t.me/ShadowKing9o"),
+                                        InlineKeyboardButton(
+                                            "Source Code", url="https://github.com/MasterShad0w/Shadow-Telegraph-Bot")
+                                    ]]
+                            ),        
+            disable_web_page_preview=True,        
+            parse_mode="html")
+
+@Jebot.on_message(filters.photo)
+async def telegraphphoto(client, message):
+    msg = await message.reply_text("Uploading To Telegraph...")
+    download_location = await client.download_media(
+        message=message, file_name='root/jetg')
+    try:
+        response = upload_file(download_location)
+    except:
+        await msg.edit_text("Photo size should be less than 5mb!") 
+    else:
+        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @ShadowsArena**',
+            disable_web_page_preview=True,
+        )
+    finally:
+        os.remove(download_location)
+
+@Jebot.on_message(filters.video)
+async def telegraphvid(client, message):
+    msg = await message.reply_text("Uploading To Telegraph...")
+    download_location = await client.download_media(
+        message=message, file_name='root/jetg')
+    try:
+        response = upload_file(download_location)
+    except:
+        await msg.edit_text("Video size should be less than 5mb!") 
+    else:
+        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @ShadowsArena**',
+            disable_web_page_preview=True,
+        )
+    finally:
+        os.remove(download_location)
+
+@Jebot.on_message(filters.animation)
+async def telegraphgif(client, message):
+    msg = await message.reply_text("Wait! Uploading To Telegraph...")
+    download_location = await client.download_media(
+        message=message, file_name='root/jetg')
+    try:
+        response = upload_file(download_location)
+    except:
+        await msg.edit_text("Gif size should be less than 5mb!") 
+    else:
+        await msg.edit_text(f'**Uploaded To Telegraph!\n\n👉 https://telegra.ph{response[0]}\n\nJoin @ShadowsArena**',
+            disable_web_page_preview=True,
+        )
+    finally:
+        os.remove(download_location)
+
+@Jebot.on_callback_query()
+async def button(bot, update):
+      cb_data = update.data
+      if "help" in cb_data:
+        await update.message.delete()
+        await help(bot, update.message)
+      elif "about" in cb_data:
+        await update.message.delete()
+        await about(bot, update.message)
+      elif "start" in cb_data:
+        await update.message.delete()
+        await start(bot, update.message)
+
+print(
+    """
+Bot Started!
+Join @ShadowsArena
+"""
+)
+
+Jebot.run()
